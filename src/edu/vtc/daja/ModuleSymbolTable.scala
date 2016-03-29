ff2b75818277ea98226339365fea043d7026b6e2
Peter C. Chapin <PChapin@vtc.vsc.edu>
2016-03-29 09:15:18 -0400

    Added skeleton type representation classes and converted the SymbolTable interface to Scala.

---
+++ src/edu/vtc/daja/ModuleSymbolTable.scala
@@ -0,0 +1,5 @@
+package edu.vtc.daja
+
+class ModuleSymbolTable {
+
+}
---
+++ src/edu/vtc/daja/TypeRep.scala
@@ -0,0 +1,17 @@
+package edu.vtc.daja
+
+object TypeRep {
+
+  abstract class Rep
+
+  // Primitive types.
+  case object BoolRep extends Rep
+  case object IntRep extends Rep
+  case object DoubleRep extends Rep
+
+  // Constructed types.
+  case class ArrayRep(elementType: Rep, size: Int) extends Rep
+  case class FunctionRep(resultType: Rep, parameters: List[Rep]) extends Rep
+  case class PointerRep(poitedAtType: Rep) extends Rep
+
+}
