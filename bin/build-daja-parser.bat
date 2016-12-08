42606e200277917396927bbc653acf83a74740b4
Peter Chapin <spicacality@gmail.com>
2016-12-08 09:53:00 -0500

    Updated project for Scala 2.12.

--- bin/build-daja-parser.bat
+++ bin/build-daja-parser.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd src\org\pchapin\daja
-java -cp ..\..\..\..\lib\antlr-4.5.1.jar org.antlr.v4.Tool -visitor Daja.g4
+java -cp ..\..\..\..\lib\antlr4-4.5.3.jar org.antlr.v4.Tool -visitor Daja.g4
 cd ..\..\..\..
 
