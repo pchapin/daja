5bcf6adcaa9c247a4c93cacee867428981153d5d
Peter Chapin <spicacality@gmail.com>
2017-01-30 17:24:31 -0500

    Fixed the grun batch file.

--- bin/grun.bat
+++ bin/grun.bat
@@ -4,5 +4,5 @@ REM This is the testing tool described in "Definitive ANTRL 4."
 REM
 
 cd build\production\Daja
-java -cp .;..\..\..\lib\antlr4-4.5.3.jar org.antlr.v4.gui.TestRig edu.vtc.daja.Daja %1 %2 %3 %4
+java -cp .;..\..\..\lib\antlr4-4.5.3.jar org.antlr.v4.gui.TestRig org.pchapin.daja.Daja %1 %2 %3 %4
 cd ..\..\..
